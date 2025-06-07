371381437833493e472de98c35e408b0c8493598
Mario Kozlovac <mario.kozlovac@outlook.com>
2025-06-07 19:28:33 +0200

    adjusteted timeout

--- PINGIP_ORG/Common/Globals.cs
+++ PINGIP_ORG/Common/Globals.cs
@@ -2,7 +2,7 @@
 {
     public static class Globals
     {
-        public static readonly TimeSpan minPingTimeSpan = TimeSpan.FromSeconds(10);
+        public static readonly TimeSpan minPingTimeSpan = TimeSpan.FromSeconds(30);
 
         public static readonly TimeSpan minPortCheckTimeSpan = TimeSpan.FromSeconds(10);
 

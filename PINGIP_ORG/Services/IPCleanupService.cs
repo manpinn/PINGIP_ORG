3063be330cbcaef5c877dcf93e2425d4f95ee1d6
Mario Kozlovac <mario.kozlovac@outlook.com>
2025-08-25 21:09:37 +0200

    refactoring ipcleanupservice

--- PINGIP_ORG/Services/IPCleanupService.cs
+++ PINGIP_ORG/Services/IPCleanupService.cs
@@ -1,59 +1,89 @@
-﻿using System;
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Concurrent;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace PINGIP_ORG.Services
 {
     public class IPCleanupService : BackgroundService
     {
-        private readonly GlobalPingIPDictionaryService _globalIPDictionary;
+        private readonly GlobalPingIPDictionaryService _globalPingIPDictionary;
+        private readonly GlobalPortCheckIPDictionaryService _globalPortCheckIPDictionary;
+        private readonly GlobalTraceRouteIPDictionaryService _globalTraceRouteIPDictionary;
         private readonly TimeSpan _cleanupInterval = TimeSpan.FromMinutes(5);
         private readonly TimeSpan _cleanupTimeSpanDate = TimeSpan.FromMinutes(3);
         private readonly ILogger<IPCleanupService> _logger;
 
-        public IPCleanupService(GlobalPingIPDictionaryService globalIPDictionary, ILogger<IPCleanupService> logger)
+        public IPCleanupService(
+            GlobalPingIPDictionaryService globalPingIPDictionary, 
+            GlobalPortCheckIPDictionaryService globalPortCheckIPDictionary,
+            GlobalTraceRouteIPDictionaryService globalTraceRouteIPDictionary,
+            ILogger<IPCleanupService> logger)
         {
-            _globalIPDictionary = globalIPDictionary;
+            _globalPingIPDictionary = globalPingIPDictionary;
+            _globalPortCheckIPDictionary = globalPortCheckIPDictionary;
+            _globalTraceRouteIPDictionary = globalTraceRouteIPDictionary;
             _logger = logger;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            while (!stoppingToken.IsCancellationRequested)
+            try
             {
-                _logger.LogInformation("Running IP cleanup at: {time}", DateTime.Now);
-
-                foreach (var item in _globalIPDictionary.GetAllRequestsFromDic())
+                while (!stoppingToken.IsCancellationRequested)
                 {
-                    DateTime timestamp = item.Value;
+                    _logger.LogInformation("Running IP cleanup at: {timestamp}", DateTime.Now);
 
-                    if ((DateTime.Now - timestamp) > _cleanupTimeSpanDate)
-                    {
-                        _globalIPDictionary.TryRemoveRequestsFromDic(item.Key);
+                    CleanupOldEntries(
+                        () => _globalPingIPDictionary.GetAllRequestsFromDic(),
+                        key => _globalPingIPDictionary.TryRemoveRequestsFromDic(key)
+                    );
 
-                        //_logger.LogInformation("Removed stale key: {key}", item.Key);
-                    }
+                    CleanupOldEntries(
+                        () => _globalPingIPDictionary.GetAllRequestsToDic(),
+                        key => _globalPingIPDictionary.TryRemoveRequestsToDic(key)
+                    );
 
+                    CleanupOldEntries(
+                        () => _globalPortCheckIPDictionary.GetAllRequestsFromDic(),
+                        key => _globalPortCheckIPDictionary.TryRemoveRequestsFromDic(key)
+                    );
+
+                    CleanupOldEntries(
+                        () => _globalPortCheckIPDictionary.GetAllRequestsToDic(),
+                        key => _globalPortCheckIPDictionary.TryRemoveRequestsToDic(key)
+                    );
+
+                    CleanupOldEntries(
+                        () => _globalTraceRouteIPDictionary.GetAllRequestsFromDic(),
+                        key => _globalTraceRouteIPDictionary.TryRemoveRequestsFromDic(key)
+                    );
+
+                    CleanupOldEntries(
+                        () => _globalTraceRouteIPDictionary.GetAllRequestsToDic(),
+                        key => _globalTraceRouteIPDictionary.TryRemoveRequestsToDic(key)
+                    );
+
+                    await Task.Delay(_cleanupInterval, stoppingToken);
                 }
-
-                foreach (var item in _globalIPDictionary.GetAllRequestsToDic())
-                {
-                    DateTime timestamp = item.Value;
-
-                    if ((DateTime.Now - timestamp) > _cleanupTimeSpanDate)
-                    {
-                        _globalIPDictionary.TryRemoveRequestsToDic(item.Key);
-
-                        //_logger.LogInformation("Removed stale key: {key}", item.Key);
-                    }
-
-                }
-
-                await Task.Delay(_cleanupInterval, stoppingToken);
             }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error occurred during IP cleanup  at: {timestamp}", DateTime.Now);
+            }
+        }
+
+        private void CleanupOldEntries(
+            Func<ConcurrentDictionary<string, DateTime>> getObjects,
+            Func<string, bool> destroyObjects)
+        {
+            getObjects()
+                .Where(item => (DateTime.Now - item.Value) > _cleanupTimeSpanDate)
+                .Select(item => destroyObjects(item.Key))
+                .ToList();
         }
     }
 }
